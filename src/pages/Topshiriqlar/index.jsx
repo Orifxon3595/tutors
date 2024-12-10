30295cf8c63536ef5a7607edee03e944f0fe91d5
Azimjon-M <azimeliboev2000@gmail.com>
2024-12-10 09:31:54 +0500

    Saidbar Topshiriqlar +

---
+++ src/pages/Topshiriqlar/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Topshiriqlar = () => {
+    return (
+        <div>Topshiriqlar</div>
+    )
+}
+
+export default Topshiriqlar
--- src/utils/sidebar.js
+++ src/utils/sidebar.js
@@ -1,60 +1,75 @@
 // icons
 import { FaUserGraduate } from "react-icons/fa6";
-import { AiOutlineBarChart, AiOutlinePieChart, AiOutlineTeam } from "react-icons/ai";
+import {
+    AiOutlineBarChart,
+    AiOutlinePieChart,
+    AiOutlineTeam,
+} from "react-icons/ai";
 import { GrUserPolice } from "react-icons/gr";
+import { RiFileList3Line } from "react-icons/ri";
 // Components
 import Analitika from "../pages/Analitika";
 import Fakultet from "../pages/Fakultet";
 import Admins from "../pages/Admins";
 import Tutors from "../pages/Tutors";
 import Baholash from "../pages/Baholash";
+import Topshiriqlar from "../pages/Topshiriqlar";
 
 const sidebar = [
-  {
-    id: 1,
-    title: "Analitka",
-    path: "/analitka",
-    icon: AiOutlinePieChart,
-    isPrivate: true,
-    element: Analitika,
-    role: ["superAdmin"],
-  },
-  {
-    id: 2,
-    title: "Fakultetlar",
-    path: "/fakultet",
-    icon: AiOutlineTeam,
-    isPrivate: true,
-    element: Fakultet,
-    role: ["superAdmin"],
-  },
-  {
-    id: 3,
-    title: "Adminlar",
-    path: "/admin",
-    icon: GrUserPolice,
-    isPrivate: true,
-    element: Admins,
-    role: ["superAdmin"],
-  },
-  {
-    id: 4,
-    title: "Tyutorlar",
-    path: "/tyutor",
-    icon: FaUserGraduate,
-    isPrivate: true,
-    element: Tutors,
-    role: ["superAdmin, admin"],
-  },
-  {
-    id: 5,
-    title: "Baholash",
-    path: "/Baholash",
-    icon: AiOutlineBarChart,
-    isPrivate: true,
-    element: Baholash,
-    role: ["superAdmin"],
-  },
-]
+    {
+        id: 1,
+        title: "Analitka",
+        path: "/analitka",
+        icon: AiOutlinePieChart,
+        isPrivate: true,
+        element: Analitika,
+        role: ["superAdmin"],
+    },
+    {
+        id: 2,
+        title: "Fakultetlar",
+        path: "/fakultet",
+        icon: AiOutlineTeam,
+        isPrivate: true,
+        element: Fakultet,
+        role: ["superAdmin"],
+    },
+    {
+        id: 3,
+        title: "Adminlar",
+        path: "/admin",
+        icon: GrUserPolice,
+        isPrivate: true,
+        element: Admins,
+        role: ["superAdmin"],
+    },
+    {
+        id: 4,
+        title: "Tyutorlar",
+        path: "/tyutor",
+        icon: FaUserGraduate,
+        isPrivate: true,
+        element: Tutors,
+        role: ["superAdmin, admin"],
+    },
+    {
+        id: 5,
+        title: "Baholash",
+        path: "/Baholash",
+        icon: AiOutlineBarChart,
+        isPrivate: true,
+        element: Baholash,
+        role: ["superAdmin"],
+    },
+    {
+        id: 6,
+        title: "Topshiriqlar",
+        path: "/topshiriqlar",
+        icon: RiFileList3Line,
+        isPrivate: true,
+        element: Topshiriqlar,
+        role: ["superAdmin"],
+    },
+];
 
 export default sidebar;
